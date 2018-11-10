56c1b63a2b7f1521d4f9341a6aae43db816f3a1a
Muzgul <Muzgul@users.noreply.github.com>
2018-11-10 14:38:52 +0200

    Final change to cam required

--- php/structure/footer.php
+++ php/structure/footer.php
@@ -1,16 +1,19 @@
 <?php
+    session_start();
     if (!isset($_SERVER['HTTP_REFERER']))
     header('Location: http://' . $_SERVER['HTTP_HOST']);
 ?>
 
-<button id="profile">Profile</button>
-<button id="feed">Feed</button>
-<button id="cam">Cam</button>
-    <?php session_start(); ?>
-        <button id="out"
+<div id="footer_nav">
+    <img id="profile" src="https://img.icons8.com/bubbles/50/000000/gender-neutral-user.png">
+    <img id="cam" src="https://img.icons8.com/dusk/50/000000/instant-camera.png">
+    <img id="feed" src="https://img.icons8.com/office/50/000000/medium-icons.png">
+</div>
+
+<img id="out" src="https://img.icons8.com/cotton/50/000000/shutdown.png"
     <?php
-    if (!(isset($_SESSION['login']) && $_SESSION['login'] != "" && $_SESSION['login'] != null)){
-        echo 'style="display: none;"';
-    }
+        if (!(isset($_SESSION['login']) && $_SESSION['login'] != "" && $_SESSION['login'] != null)){
+            echo 'style="display: none;"';
+        }
     ?>
-    >Logout</button>
+>
